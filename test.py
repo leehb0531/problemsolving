c9cd5e2a5903ed49e42c89dbf5fd929a43f848d8
Hongbin Lee <ksmlee0531@gmail.com>
2020-08-14 17:36:37 -1000

    solving Mysql problems

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
-def solution(n):
-    f0 =0
-    f1 =1
-    fn = fibo(n)
-    print(fn)
-    print("done")
-
-def fibo(n):
-    if n == 0:
-        return 0
-    elif n== 1:
-        return 1
-    elif (n>=3):
-        fn = fibo(n-1)+fibo(n-2)
-        return fn
-
-solution(500)
+n = input()
+space = " "
+star = "*"
+numOfSpace = int(n)-1
+print("Type a number: "+ n)
+for i in range(1,int(n)+1):
+    numOfStar = 2*i-1
+    print(space*numOfSpace+star*numOfStar)
+    numOfSpace -= 1
