7ba73f82c8bc2e100e3d6ce50af12f9c641ee37e
Hongbin Lee <ksmlee0531@gmail.com>
2020-09-09 18:56:41 +0900

    2 baekjoon, 1 hackerRank

---
+++ Baekjoon/2739_baekjoon.py
@@ -0,0 +1,5 @@
+# 구구단
+
+x = input()
+for i in range(1,10):
+    print("%d * %d = %d"%(int(x),i, int(x)*i))
---
+++ Baekjoon/2902_baekjoon.py
@@ -0,0 +1,8 @@
+# KMP는 왜 KMP일까?
+
+x = input().split("-")
+a=""
+for i in range(len(x)):
+    t = x[i]
+    a += t[0]
+print(a)
